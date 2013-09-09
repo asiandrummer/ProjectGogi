5d384a935a0ca39fe48f210a732103e1ea6c7a0c
Hyo Jeong <asiandrummer@gmail.com>
2013-09-09 01:52:52 -0700

    Project GOGI starts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectGogi::Application.config.secret_key_base = '3d495c53b53fc039ad90701add6861091cd69e9ecc889347793d21405bb0fe8ca975eae52b05291ec3b432d21f3b70fe4c327d60adb6733b45e2269686276d54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectGogi::Application.config.session_store :cookie_store, key: '_project-gogi_session'
