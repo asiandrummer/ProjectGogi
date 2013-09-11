2d0d890b4c9f941b5bfb63de11451646acb01e07
Hyo Jeong <asiandrummer@gmail.com>
2013-09-11 01:18:31 -0700

    Project Gogi static pages start

--- Gemfile
+++ Gemfile
@@ -1,11 +1,19 @@
 source 'https://rubygems.org'
 
+ruby '2.0.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
 # Use sqlite3 as the database for Active Record
-group :development do
+group :development, :test do
     gem 'sqlite3', '1.3.7'
+    gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+    gem 'selenium-webdriver', '2.35.1'
+    gem 'capybara', '2.1.0'
 end
 
 # Use SCSS for stylesheets
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ProjectGogi::Application.config.secret_key_base = '3d495c53b53fc039ad90701add6861091cd69e9ecc889347793d21405bb0fe8ca975eae52b05291ec3b432d21f3b70fe4c327d60adb6733b45e2269686276d54'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+ProjectGogi::Application.config.secret_key_base = secure_token
