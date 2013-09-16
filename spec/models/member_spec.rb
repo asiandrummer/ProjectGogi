ea3388c6f58372455f2bbcccfad4a3aaec2c0653
Hyo Jeong <asiandrummer@gmail.com>
2013-09-15 20:01:31 -0700

    Deleted api.rb since it introduces the circular dependency.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Member do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before {
+		@member = Member.new(name: "John Doe", user_id: "1")
+	}
+
+	subject { @member }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:user_id) }
+
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before { @member.name = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when user_id is not present" do
+		before { @member.user_id = " " }
+		it { should_not be_valid }
+	end
+
 end
